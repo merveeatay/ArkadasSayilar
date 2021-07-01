5153686c375475a5089946cc3e9e0d70ea736c2c
merveeatay <83241309+merveeatay@users.noreply.github.com>
2021-07-01 20:49:23 +0300

    ArkadasSayilar
    ArkadasSayilar

---
+++ ödev5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ödev5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 220;
+            int sayi2 = 284;
+            int toplam1 = 0;
+            int toplam2 = 0;
+            for (int i = 1; i < sayi1; i++) {
+                if (sayi1 % i == 0)
+                {
+
+                    toplam1 += i;
+                }
+
+                }
+
+                for (int i=1;i<sayi2;i++){
+                if (sayi2 % i == 0) {
+                    toplam2 += i;
+            }
+            }
+            if (sayi1 == toplam2 && sayi2 == toplam1) {
+                Console.WriteLine("Sayılar Arkadaş sayılardır.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Sayılar Arkadaş sayı değildir");
+                Console.ReadLine();
+            }
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
